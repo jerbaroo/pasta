69c1f86e129095547d97e37cbe44b8e5235bdbc4
jerbaroo <jerbaroo.work@pm.me>
2024-10-23 14:08:43 +0100

    Development environment

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    purescript-overlay = {
+      url = "github:thomashoneyman/purescript-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = builtins.attrValues self.overlays;
+      });
+    in {
+      overlays = { purescript = inputs.purescript-overlay.overlays.default; };
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs
+              purs
+              purs-backend-es
+              purs-tidy-bin.purs-tidy-0_10_0
+              spago-unstable
+            ];
+          };
+        }
+      );
+  };
+}
